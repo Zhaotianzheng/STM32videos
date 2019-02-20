1ec11dca4971c31fba15165e29e3f17d5abb7f6b
SausageCode <matej.blagsic@protonmail.com>
2019-02-20 21:19:14 +0100

    update on 5th video files

--- VIDEO5_ADC/CODE/main.c
+++ VIDEO5_ADC/CODE/main.c
@@ -6,30 +6,30 @@
 void RCC_Setup();
 void GPIO_Setup();
 void USART1_Setup();
-void ADC_Setup();
+void ADC1_Setup();
 
 // Other function prototypes
 void USART_SendText(volatile char *s);
 void USART_SendNumber(uint32_t x);
 
-float ADCData; //raw ADC1 data
-char buffer[10]; //buffer for float number
+float adcdata;
+char buffer[10];
 
 int main(void)
 {
   RCC_Setup();
   GPIO_Setup();
   USART1_Setup();
-  ADC_Setup();
+  ADC1_Setup();
 
   while(1)
   {
-    while(!ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC)); // wait for data
-    ADCData = (float)ADC_GetConversionValue(ADC1);
-    ADCData = ADCData * 3.3 / (float)(1 << 12); // convert to volts
-    sprintf(buffer, "%f", ADCData); // put float number into a string
-    USART_SendText(buffer); // send number
-    USART_SendText("\n");
+    while(!ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC)); // wati for data
+    adcdata = (float)ADC_GetConversionValue(ADC1); // save data
+    adcdata = adcdata * 3.3 / (1 << 12); // convert to volts
+    sprintf(buffer, "%f", adcdata); // convert float into string
+    USART_SendText(buffer); // send data
+    USART_SendText("\n"); // send new line command
   }
 }
 
@@ -47,18 +47,20 @@ void GPIO_Setup()
   // Init LED on GPIOD
   GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
   GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
   GPIO_Init(GPIOD, &GPIO_InitStruct);
 
   // Init GPIOB pins for USART
   GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_6 | GPIO_Pin_7;
   GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
-  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_Init(GPIOB, &GPIO_InitStruct);
   // Connect pins to AF pins of USART1
   GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_USART1);
   GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_USART1);
 
-  // Init GPIOA for ADC
+  // Init GPIOA for analog in for ADC1
   GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_0;
   GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AN;
   GPIO_Init(GPIOA, &GPIO_InitStruct);
@@ -81,33 +83,31 @@ void USART1_Setup()
   USART_SendText("TESTING\n");
 }
 
-/* This function sets up ADC1 peripheral */
-void ADC_Setup()
+/* This function starts ADC1 peripheral */
+void ADC1_Setup()
 {
-  ADC_InitTypeDef ADC_InitStruct;
   ADC_CommonInitTypeDef ADC_CommonInitStruct;
+  ADC_InitTypeDef ADC_InitStruct;
 
-  // Setup global settings for ADCs
   ADC_CommonInitStruct.ADC_Mode = ADC_Mode_Independent;
   ADC_CommonInitStruct.ADC_Prescaler = ADC_Prescaler_Div2;
   ADC_CommonInitStruct.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;
   ADC_CommonInitStruct.ADC_TwoSamplingDelay = ADC_TwoSamplingDelay_5Cycles;
   ADC_CommonInit(&ADC_CommonInitStruct);
 
-  // Setup ADC1
   ADC_InitStruct.ADC_Resolution = ADC_Resolution_12b;
   ADC_InitStruct.ADC_DataAlign = ADC_DataAlign_Right;
   ADC_InitStruct.ADC_ScanConvMode = DISABLE;
-  ADC_InitStruct.ADC_ContinuousConvMode = ENABLE;
+  ADC_InitStruct.ADC_ContinuousConvMode = ENABLE; // continue making conversions
   ADC_InitStruct.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
   ADC_InitStruct.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T1_CC1;
   ADC_InitStruct.ADC_NbrOfConversion = 1;
   ADC_Init(ADC1, &ADC_InitStruct);
 
-  ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_144Cycles); // use this pin
+  ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_144Cycles); // input is channel 0
 
-  ADC_Cmd(ADC1, ENABLE); //start ADC1
-  ADC_SoftwareStartConv(ADC1); //start ADC1 conversions
+  ADC_Cmd(ADC1, ENABLE); // Start ADC1
+  ADC_SoftwareStartConv(ADC1); // Start ADC1 conversions
 }
 
 /* This functions send text or char array passed as argument over USART */
